ec94297cb87c217e66ace8183cd5a2d9ea845661
davidaxel121 <davidaxel121@gmail.com>
2021-02-12 00:49:31 -0600

    Adding initial Gatsby template

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import Hero from '../components/hero';
+
+const IndexPage = () => {
+	return (
+		<main className="bg-gray-100">
+			<Navbar />
+			<Hero />
+		</main>
+	)
+}
+
+export default IndexPage;
